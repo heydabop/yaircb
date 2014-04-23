c249371eb7fee603df26f0b7ed22eb55889a0675
heydabop <heydabop@gmail.com>
2014-04-23 09:34:59 -0500

    connected and joined a channel

--- irc.go
+++ irc.go
@@ -4,7 +4,19 @@ import(
 	"fmt"
 	"net/textproto"
 	"time"
+	"sync"
 )
+
+func readToConsole(socket *textproto.Conn, wg sync.WaitGroup){
+	line, line_err := socket.Reader.ReadLine()
+	for ; line_err == nil; line, line_err = socket.Reader.ReadLine() {
+		fmt.Println(line)
+	}
+	if line_err != nil {
+		fmt.Println("LINE ERROR: ", line_err.Error())
+	}
+	wg.Done()
+}
 
 func main(){
 	socket, err := textproto.Dial("tcp", "irc.tamu.edu:6667")
@@ -17,12 +29,16 @@ func main(){
 		fmt.Println("WRITE ERROR: ", write_err)
 	}
 	time.Sleep(3 * time.Second)
-	var line string
-	var line_err error
-	for line, line_err = socket.Reader.ReadLine(); line_err == nil; line, line_err = socket.Reader.ReadLine() {
-		fmt.Println(line)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go readToConsole(socket, wg)
+	write_err = socket.Writer.PrintfLine("USER yaircb * * gobot")
+	if write_err != nil {
+		fmt.Println("WRITE ERROR: ", write_err)
 	}
-	if line_err != nil {
-		fmt.Println("LINE ERROR: ", line_err.Error())
+	write_err = socket.Writer.PrintfLine("JOIN #tacs")
+	if write_err != nil {
+		fmt.Println("WRITE ERROR: ", write_err)
 	}
+	wg.Wait()
 }
