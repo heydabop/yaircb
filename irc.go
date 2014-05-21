474dc6d544aaedc2fe419b9c3a9d9f515c111bdb
heydabop <heydabop@gmail.com>
2014-05-21 18:25:40 -0500

    comments

--- irc.go
+++ irc.go
@@ -29,19 +29,24 @@ func errOut(err error, quit chan bool) {
 //take input from srvChan and send to server
 func writeToServer(socket *textproto.Conn, srvChan chan string, wg *sync.WaitGroup, quit chan bool) {
 	defer wg.Done()
-	defer fmt.Println("WTS")
+	defer fmt.Println("WTS") //debug
+
 	w := socket.Writer
 	err := w.PrintfLine(<-srvChan)
+	//send all lines in srvChan to server
 	for err == nil {
 		select {
-		case <-quit:
+		case <-quit: //exit if indicated
 			return
 		case str := <-srvChan:
 			err = w.PrintfLine(str)
 		}
 	}
+
+	//print error and exit
 	if err != nil {
 		errOut(err, quit)
+		quit <- true //errOut might not do this, need to change error handling
 		socket.Close()
 	}
 }
@@ -49,24 +54,30 @@ func writeToServer(socket *textproto.Conn, srvChan chan string, wg *sync.WaitGro
 //take input from connection and write out to console, also handle PING/PONG
 func writeToConsole(socket *textproto.Conn, srvChan chan string, wg *sync.WaitGroup, quit chan bool) {
 	defer wg.Done()
-	defer fmt.Println("WTC")
+	defer fmt.Println("WTC") //debug
+
 	r := socket.Reader
 	pingRegex := regexp.MustCompile("^PING (.*)")
 	line, line_err := r.ReadLine()
+	//read every line from the server and print to console
 	for ; line_err == nil; line, line_err = r.ReadLine() {
 		select {
-		case <-quit:
+		case <-quit: //exit if indicated
 			return
 		default:
-		}
-		fmt.Println(line)
-		if match := pingRegex.FindStringSubmatch(line); match != nil {
-			srvChan <- ("PONG " + match[1])
-			fmt.Println("PONG", match[1])
+			fmt.Println(line)
+			if match := pingRegex.FindStringSubmatch(line); match != nil {
+				//respond to PING from server
+				srvChan <- ("PONG " + match[1])
+				fmt.Println("PONG", match[1]) //put to console
+			}
 		}
 	}
+
+	//print error and exit
 	if line_err != nil {
 		errOut(line_err, quit)
+		quit <- true //errOut might not do this, need to change error handling
 		socket.Close()
 	}
 }
@@ -74,17 +85,22 @@ func writeToConsole(socket *textproto.Conn, srvChan chan string, wg *sync.WaitGr
 //read input from console and send to srvChan
 func readFromConsole(srvChan chan string, wg *sync.WaitGroup, quit chan bool, error chan bool) {
 	defer wg.Done()
-	defer fmt.Println("RFC")
+	defer fmt.Println("RFC") //debug
+
 	in := bufio.NewScanner(os.Stdin)
+	//read all text from console, send it to srvChan to be sent to server
 	for in.Scan() {
 		str := in.Text()
 		srvChan <- str
-		if strings.TrimSpace(str) == "QUIT" {
+		if strings.TrimSpace(str) == "QUIT" { //exit and indicate upon reading QUIT
 			error <- true
 			return
 		}
 	}
+
+	//print error and exit
 	if err := in.Err(); err != nil {
+		error <- true
 		errOut(err, quit)
 	}
 }
@@ -92,9 +108,10 @@ func readFromConsole(srvChan chan string, wg *sync.WaitGroup, quit chan bool, er
 func main() {
 	//funcMap := initMap()
 	srvChan := make(chan string)
+	//wgSrv for goroutines to/from sever, wg for readFromConsole
 	var wgSrv, wg sync.WaitGroup
-	quit := make(chan bool, 2)
-	error := make(chan bool, 1)
+	quit := make(chan bool, 2)  //used to indicate server to/from goroutines need to exit
+	error := make(chan bool, 1) //used to indicate readFromConsole exited
 	//initiate connection
 	wg.Add(1)
 	go readFromConsole(srvChan, &wg, quit, error) //doesnt get restarted on connection EOF
